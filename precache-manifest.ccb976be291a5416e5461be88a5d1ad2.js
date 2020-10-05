b15c993e60840d11084d11600d18a3826e3307e7
pratim440 <c.pratim440@gmail.com>
2020-10-05 17:44:52 +0530

    Updates

---
+++ precache-manifest.ccb976be291a5416e5461be88a5d1ad2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "642f5ece37a9206441a2b2d7bb1a2873",
+    "url": "/react_weather_app/index.html"
+  },
+  {
+    "revision": "f0f27d48bb99933f4e8e",
+    "url": "/react_weather_app/static/css/main.0140b274.chunk.css"
+  },
+  {
+    "revision": "46845e073faca4a40c6f",
+    "url": "/react_weather_app/static/js/2.fe3f6dfb.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react_weather_app/static/js/2.fe3f6dfb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0f27d48bb99933f4e8e",
+    "url": "/react_weather_app/static/js/main.751d727a.chunk.js"
+  },
+  {
+    "revision": "f898914bdeb918ce1ba6",
+    "url": "/react_weather_app/static/js/runtime-main.970bf947.js"
+  },
+  {
+    "revision": "41dce73704e52732b3033fb3a69a57be",
+    "url": "/react_weather_app/static/media/video.41dce737.mp4"
+  }
+]);
